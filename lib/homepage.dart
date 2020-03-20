af71b579b52308ed61c9ee26bcc700d1a0daa916
gabssanto <espiritosanto.gabriel@gmail.com>
2020-03-20 16:49:20 -0300

    creating app

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatelessWidget {
+  _funct() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.DENIED) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.GRANTED) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    print(_locationData.latitude);
+    print(_locationData.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bom Dia'),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            child: Text('teste'),
+            onPressed: () {
+              var location = _funct();
+            },
+          ),
+        ],
       ),
     );
   }
