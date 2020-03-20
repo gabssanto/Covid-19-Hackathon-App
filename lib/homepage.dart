383d802138eae5f886ecc7ec252b4a0150ee5e4d
gabssanto <espiritosanto.gabriel@gmail.com>
2020-03-20 20:13:24 -0300

    creating app

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Timer timer;
+
   _funct() async {
     Location location = new Location();
 
@@ -26,23 +35,35 @@ class HomePage extends StatelessWidget {
     }
 
     _locationData = await location.getLocation();
-    print(_locationData.latitude);
-    print(_locationData.longitude);
+    print('Latitude: ${_locationData.latitude}');
+    print('Longitude: ${_locationData.longitude}');
+    print('Accuracy: ${_locationData.accuracy}');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _funct();
+    timer = Timer.periodic(Duration(seconds: 15), (Timer t) => _funct());
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bom Dia'),
+        title: Text('Home'),
       ),
       body: Column(
         children: <Widget>[
           RaisedButton(
             child: Text('teste'),
-            onPressed: () {
-              var location = _funct();
-            },
+            onPressed: () {},
           ),
         ],
       ),
