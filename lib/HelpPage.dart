a257bce81ff74ac31e19e9066eccf531c926832f
gabssanto <espiritosanto.gabriel@gmail.com>
2020-03-22 16:54:48 -0300

    mapas funcionando corretamente

--- lib/HelpPage.dart
+++ lib/HelpPage.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'dart:core';
 import 'dart:async';
 import 'package:covid19/mobx/imports.dart';
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HelpPage extends StatefulWidget {
@@ -15,20 +18,32 @@ class _HelpPageState extends State<HelpPage> {
     teste();
     super.initState();
   }
-
   //https://i3geo.saude.gov.br/i3geo/ogc.php?service=WFS&version=1.0.0&request=GetFeature&typeName=ubs_funcionamento&outputFormat=JSON
   Future<void> teste () async {
-    String data = await DefaultAssetBundle.of(context).loadString("assets/dataframe.json");
-    final jsonResult = json.decode(data);
-    var latitude = jsonResult['lat'];
-    var longitude = jsonResult['long'];
-    var name = jsonResult['no_fantasia'];
-    var place = jsonResult['no_logradouro'];
-    var number = jsonResult['nu_telefone'];
-    var cep = jsonResult['co_cep'];
-    var uf = jsonResult['uf'];
-    var city = jsonResult['cidade'];
-    print(latitude);
+    String latitudes = await DefaultAssetBundle.of(context).loadString('assets/lat.txt');
+    String longitudes = await DefaultAssetBundle.of(context).loadString('assets/long.txt');
+    String no_fantasias = await DefaultAssetBundle.of(context).loadString('assets/no_fantasia.txt');
+    String no_logradouros = await DefaultAssetBundle.of(context).loadString('assets/no_logradouro.txt');
+    String nu_telefone = await DefaultAssetBundle.of(context).loadString('assets/nu_telefone.txt');
+    String co_ceps = await DefaultAssetBundle.of(context).loadString('assets/co_cep.txt');
+    String ufs = await DefaultAssetBundle.of(context).loadString('assets/uf.txt');
+    String cidades = await DefaultAssetBundle.of(context).loadString('assets/cidade.txt');
+
+
+    //String data = await DefaultAssetBundle.of(context).loadString("assets/dataframe.json");
+    //final jsonResult = json.decode(data);
+    handleLocations.setUBS(latitudes.split(','), longitudes.split(','), no_fantasias.split(','), no_logradouros.split(','), nu_telefone.split(','), co_ceps.split(','), ufs.split(','), cidades.split(','));
+    //print(handleLocations.ubsNames.length);
+    /*handleLocations.setUBS(
+        jsonResult['lat'],
+        jsonResult['long'],
+        jsonResult['no_fantasia'],
+        jsonResult['no_logradouro'],
+        jsonResult['nu_telefone'],
+        jsonResult['co_cep'],
+        jsonResult['uf'],
+        jsonResult['cidade']
+    );*/
   }
   
   @override
@@ -48,36 +63,88 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
+  static final CameraPosition _initialPosition = CameraPosition(
     target: LatLng(handleLocations.latitude, handleLocations.longitude),
     zoom: handleLocations.accuracy,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
+  static final CameraPosition _currentPosition = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(handleLocations.latitude, handleLocations.longitude),
       zoom: handleLocations.accuracy);
+
+  Set<Marker> _markers = {};
+
+  BitmapDescriptor pinLocationIcon;
+
+  @override
+  void initState() {
+    BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5),
+        'assets/pin.png').then((onValue) {
+      pinLocationIcon = onValue;
+    });
+    super.initState();
+  }
+
+/*
+  loadAsset() async {
+    final myData = await rootBundle.loadString("assets/dataframe.csv");
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
+
+    data = csvTable;
+    print(data.map((e) {
+      return e;
+    }).toList()[1][1]);
+  }
+*/
+
+  Set<Marker> _createMarker() {
+    return <Marker>[
+      Marker(
+        markerId: MarkerId("marker_1"),
+        position: LatLng(handleLocations.latitude, handleLocations.longitude),
+        icon: pinLocationIcon,
+      ),
+    ].toSet();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        myLocationEnabled: true,
+        markers: _markers,
+        myLocationButtonEnabled: true,
+        buildingsEnabled: false,
+        mapType: MapType.normal,
+        initialCameraPosition: _initialPosition,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
-        },
+          setState(() {
+            for(int i = 0; i < handleLocations.ubsNames.length-1; i++) {
+              double minusLat = handleLocations.latitude - double.parse(handleLocations.ubsLatitudes[i]);
+              double minusLong = handleLocations.longitude - double.parse(handleLocations.ubsLongitudes[i]);
+              if(minusLat.abs() <= 0.1 && minusLong.abs() <= 0.1) {
+                _markers.add(
+                    Marker(
+                      markerId: MarkerId(handleLocations.ubsNames[i]),
+                      position: LatLng(double.parse(handleLocations.ubsLatitudes[i]), double.parse(handleLocations.ubsLongitudes[i])),
+                      icon: pinLocationIcon,
+                    )
+                );
+              }
+            }
+          });
+          },
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        onPressed: _goToTheLake,
-        child: Icon(Icons.my_location, color: Colors.black54,),
-      ),
+
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToCurrentPosition() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(_currentPosition));
   }
 }
