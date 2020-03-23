1d481eda0d98409f7f9b4aac34e33f1eaa859c0c
gabssanto <espiritosanto.gabriel@gmail.com>
2020-03-23 00:40:25 -0300

    paginas quase prontas

---
+++ lib/global/RedDialog.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class RedDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(21)
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width - 80,
+        height: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text("ATENÇÃO",
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  color: Colors.black54,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  letterSpacing: -0.176,
+
+                )
+            ),
+            Text("Você é um caso confirmado",
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  color: Color(0xfff65f68),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  letterSpacing: -0.176,
+
+                )
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Text("Se seus sintomas são leves é orientado que permaneça em isolamento residencial e somente procure ajuda médica se houver agravamento dos sintomas. Em caso de dúvidas, ligue para o Dique Saúde 136 do Ministério da Saúde.",
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Colors.black54,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.normal,
+                      letterSpacing: -0.154,
+
+                    )
+                )
+
+            ),
+            Container(
+              width: 285,
+              height: 49,
+              decoration: new BoxDecoration(
+                  color: Color(0xfff65f68),
+                  borderRadius: BorderRadius.circular(14)
+              ),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Fechar",
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.normal,
+                          letterSpacing: -0.165,
+
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            )
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -15,34 +15,297 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Container(
         height: double.infinity,
         color: Color(0xffefefef),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            Container(
-                width: MediaQuery.of(context).size.width - 50,
-                height: MediaQuery.of(context).size.height - 150,
-                decoration: new BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12)
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.account_circle, size: 60, color: Colors.black54,),
-                    Text("Gabriel Alves",
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          color: Color(0xff27b3ff),
-                          fontSize: 21,
-                          fontWeight: FontWeight.w700,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: -0.231,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  width: MediaQuery.of(context).size.width - 50,
+                  height: MediaQuery.of(context).size.height - 120,
+                  decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12)
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 5,bottom: 5),
+                        child: Icon(Icons.account_circle, size: 80, color: Colors.black54,),
+                      ),
+                      Text("Gabriel Alves",
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Color(0xff27b3ff),
+                            fontSize: 21,
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.normal,
+                            letterSpacing: -0.231,
 
-                        )
-                    )
+                          )
+                      ),
+                      Text("Supeito de Covid-19",
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Color(0xffffb027),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            fontStyle: FontStyle.normal,
+                            letterSpacing: -0.165,
 
-                  ],
+                          )
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 15, left: 20, right: 20),
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Informações Básicas",
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    color: Color(0xff27b3ff),
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                    fontStyle: FontStyle.normal,
+                                    letterSpacing: -0.165,
+                                  )
+                              ),
+                              Container(
+                                  margin: EdgeInsets.only(top: 5),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height / 20,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: new InputDecoration(
+                                      labelText: 'Nome',
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Color(0xff27b3ff), width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: Colors.grey, width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      prefixIcon: Padding(
+                                        padding: EdgeInsets.only(top: 0),
+                                        // add padding to adjust icon
+                                        child: Icon(Icons.account_box),
+                                      ),
+                                    ),
+                                  )),
+                              Container(
+                                  margin: EdgeInsets.only(top: 15),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height / 20,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: new InputDecoration(
+                                      labelText: 'Sobrenome',
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Color(0xff27b3ff), width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: Colors.grey, width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      prefixIcon: Padding(
+                                        padding: EdgeInsets.only(top: 0),
+                                        // add padding to adjust icon
+                                        child: Icon(Icons.account_box),
+                                      ),
+                                    ),
+                                  )),
+                              Container(
+                                  margin: EdgeInsets.only(top: 15),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height / 20,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: new InputDecoration(
+                                      labelText: 'Idade',
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Color(0xff27b3ff), width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: Colors.grey, width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      prefixIcon: Padding(
+                                        padding: EdgeInsets.only(top: 0),
+                                        // add padding to adjust icon
+                                        child: Icon(Icons.cake),
+                                      ),
+                                    ),
+                                  )),
+                              Container(
+                                  margin: EdgeInsets.only(top: 15),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height / 20,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: new InputDecoration(
+                                      labelText: 'Idade',
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Color(0xff27b3ff), width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: Colors.grey, width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      prefixIcon: Padding(
+                                        padding: EdgeInsets.only(top: 0),
+                                        // add padding to adjust icon
+                                        child: Icon(Icons.phone),
+                                      ),
+                                    ),
+                                  )),
+                              Divider(),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin: EdgeInsets.only(top: 0, bottom: 0),
+                                decoration: BoxDecoration(),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Container(
+                                      child: Text("Localização",
+                                          style: TextStyle(
+                                            fontFamily: 'Montserrat',
+                                            color: Color(0xff27b3ff),
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w600,
+                                            fontStyle: FontStyle.normal,
+                                            letterSpacing: -0.165,
+
+                                          )
+                                      ),
+                                    ),
+                                    Icon(Icons.keyboard_arrow_right, color: Color(0xff27b3ff),),
+                                  ],
+                                ),
+                              ),
+                              Divider(),
+                              Container(
+                                child: Text("Segurança",
+                                    style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      color: Color(0xff27b3ff),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600,
+                                      fontStyle: FontStyle.normal,
+                                      letterSpacing: -0.165,
+                                    )
+                                ),
+                              ),
+                              Container(
+                                  margin: EdgeInsets.only(top: 15),
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height / 20,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: new InputDecoration(
+                                      labelText: 'Senha',
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Color(0xff27b3ff), width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: Colors.grey, width: 1.0),
+                                          borderRadius: BorderRadius.circular(12)),
+                                      prefixIcon: Padding(
+                                        padding: EdgeInsets.only(top: 0),
+                                        // add padding to adjust icon
+                                        child: Icon(Icons.lock),
+                                      ),
+                                    ),
+                                  )),
+                              Container(
+                                margin: EdgeInsets.only(top: 5, bottom: 15),
+                                child: Text("Digite aqui para mudar a senha",
+                                    style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      color: Colors.black38,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600,
+                                      fontStyle: FontStyle.normal,
+                                      letterSpacing: -0.132,
+
+                                    )
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 10),
+                                width: MediaQuery.of(context).size.width,
+                                height: MediaQuery.of(context).size.height / 18,
+                                decoration: new BoxDecoration(
+                                    color: Color(0xff27b3ff),
+                                    border: Border.all(color: Color(0xff27b3ff)),
+                                    borderRadius: BorderRadius.circular(13)
+                                ),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text("Salvar",
+                                        style: TextStyle(
+                                          fontFamily: 'Montserrat',
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          fontStyle: FontStyle.normal,
+                                          letterSpacing: -0.176,
+
+                                        )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height / 18,
+                                  decoration: new BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border.all(color: Color(0xff27b3ff)),
+                                      borderRadius: BorderRadius.circular(13)
+                                  ),
+                                  child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text("Sair da conta",
+                                          style: TextStyle(
+                                            fontFamily: 'Montserrat',
+                                            color: Color(0xff27b3ff),
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w600,
+                                            fontStyle: FontStyle.normal,
+                                            letterSpacing: -0.176,
+
+                                          )
+                                      ),
+                                    ],
+                                  ),
+                              )
+
+
+                            ],
+                          )
+                      )
+
+                    ],
+                  ),
                 ),
+              ],
             ),
           ],
         )
