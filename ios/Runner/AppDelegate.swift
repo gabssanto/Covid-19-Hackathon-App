8c1a67cddf11c50e2c251c9bb39de41083b368ec
gabssanto <espiritosanto.gabriel@gmail.com>
2020-04-04 15:47:24 -0300

    firebase removido e agora fuciona no IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCXJBDTyUwD2fgkhsdj8LskYOp20qwHjrQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/mobx/handleUser.dart
+++ lib/mobx/handleUser.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:async';
 
 import 'package:mobx/mobx.dart';
@@ -52,47 +51,6 @@ abstract class _HandleUserBase with Store {
     this.street = street;
     this.numberOfPeople = numberOfPeople;
     this.password = password;
-    signUp();
   }
 
-  @action
-  login(email, password) async {
-    String res = await signIn(email, password);
-    this.userID = res;
-  }
-
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
-  Future<String> signIn(String email, String password) async {
-    AuthResult result = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password);
-    FirebaseUser user = result.user;
-    return user.uid;
-  }
-
-  Future<String> signUp() async {
-    AuthResult result = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: this.email, password: this.password);
-    FirebaseUser user = result.user;
-    return user.uid;
-  }
-
-  Future<FirebaseUser> getCurrentUser() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    return user;
-  }
-
-  Future<void> signOut() async {
-    return _firebaseAuth.signOut();
-  }
-
-  Future<void> sendEmailVerification() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    user.sendEmailVerification();
-  }
-
-  Future<bool> isEmailVerified() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    return user.isEmailVerified;
-  }
 }
